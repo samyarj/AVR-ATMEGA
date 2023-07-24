4fff89f2fd54301be43704777ad207e133e99acb
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2023-07-24 18:10:59 -0400

    correcting types

---
+++ library_refactored/AbsPosition.h
@@ -0,0 +1,26 @@
+#include <avr/io.h>
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <memory>
+
+using namespace std;
+
+class AbsPosition
+{
+public:
+    AbsPosition(uint32_t x, uint32_t y) : x_(x), y_(y){}
+    
+    uint32_t getPosX() { return x_; }
+    uint32_t getPosY() { return y_; }
+    
+    void setPosX(uint32_t x) { x_ = x; }
+    void setPosY(uint32_t y) { y_ = y; }
+
+private:
+    uint32_t x_;
+    uint32_t y_;
+};
