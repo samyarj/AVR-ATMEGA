148e15ee5cd1abf0956a50efce8205584da7749c
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2023-07-24 18:45:41 -0400

    adjusting class tile and absposition

--- library_refactored/Tile.cpp
+++ library_refactored/Tile.cpp
@@ -2,9 +2,14 @@
 
 void Tile::setNeighbour(uint8_t angle, int idNeighbour)
 {
-    if (angleToIndex_.count(angle) > 0) {
+    if (angleToIndex_.count(angle) == 1) {
         neighbours_[angleToIndex_.at(angle)] = idNeighbour;
     }
     else
         cout << "invalide input" << endl; 
+}
+
+int getNeighbourKey(uint16_t angle)
+{
+    return neighbours_[angleToIndex_.at(angle)];
 }
