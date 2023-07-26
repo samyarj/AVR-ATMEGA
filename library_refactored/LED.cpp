91896818c1d430b3475d2708d707be8dd5ae6fd8
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2023-07-26 13:34:55 -0400

    completion of LED class and adding comment

--- library_refactored/LED.cpp
+++ library_refactored/LED.cpp
@@ -10,17 +10,45 @@ LED::~LED()
     activateInMode();
 }
 
-void LED::eteindre()
+void LED::turnOff()
 {
     togglePort(_mode00);
 }
 
-void LED::allumerRouge()
+void LED::turnRed()
 {
     togglePort(_mode01);
 }
 
-void LED::allumerVert()
+void LED::turnGreen()
 {
     togglePort(_mode10);
+}
+
+uint32_t LED::calculateIterations(uint32_t delay, uint32_t unity)
+{
+    return (_LEDFrequency * delay) / unity;
+}
+
+void LED::turnAmber(uint32_t iterations)
+{
+    for (uint32_t i{0}; i < iterations; i++) //250us per cycle
+    {
+      turnRed();
+      _delay_us(31);
+      turnGreen();
+      _delay_us(73);
+      _delay_us(73);
+      _delay_us(73); //repeat 3 times since the precision of this function decreases for values over 100
+    }
+}
+
+void LED::turnAmberMs(uint32_t delay)
+{
+    turnAmber(calculateIterations(delay, 1000)); //for a delay in milliseconds
+}
+
+void LED::turnAmberS(uint32_t delay)
+{
+    turnAmber(calculateIterations(delay, 1)); //for a delay in seconds
 }
--- library_refactored/LED.h
+++ library_refactored/LED.h
@@ -6,15 +6,23 @@ public:
     LED();
     virtual ~LED();
 
-    void eteindre();
-    void allumerRouge();
-    void allumerVert();
+    void turnOff();
+    void turnRed();
+    void turnGreen();
 
+    void turnAmber(uint32_t iterations);
 
+    void turnAmberMs(uint32_t delay); //delay in milliseconds
+    void turnAmberS(uint32_t delay); //delay in seconds
 
 protected:
+    uint32_t calculateIterations(uint32_t delay, uint32_t unity);
+    
     volatile uint8_t *_port = &PORTA;
     volatile uint8_t *_ddr = &DDRA;
     uint8_t _bit = 0;
 
+private:
+    uint32_t _LEDFrequency = 4000; //constant for the calculateIterations
+    //4000Hz, since the cycle red-green of the turnAmber method takes 250us (4000*250*10⁻6 = 1 sec)
 };
