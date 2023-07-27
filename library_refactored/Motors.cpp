e26c41bd19e20b9c52cf042bec06b6c50dea0b77
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2023-07-27 13:26:25 -0400

    completion of the motors module

--- library_refactored/Motors.cpp
+++ library_refactored/Motors.cpp
@@ -1,11 +1,19 @@
 #include "Motors.h"
 
-Motors::Motors()
+Motors::Motors(Frequency frequency) : IOPorts()
 {
     activateOutMode();
+    selectPWMFrequency(frequency);
     // division d'horloge par 8 - implique une fréquence de PWM fixe
     TCCR0A |= (1 << WGM00);
     TCCR0A |= (1 << COM0A1) | (1 << COM0B1);
+}
+
+Motors::~Motors()
+{
+    activateInMode();
+    TCCR0A |= (0 << WGM00);
+    TCCR0A |= (0 << COM0A1) | (0 << COM0B1);
 }
 
 void Motors::setIOMode(uint8_t wantedMode)
@@ -20,24 +28,79 @@ void Motors::chooseMovement(uint8_t leftWheelDirection, uint8_t rightWheelDirect
     setPort(_port, _bit + 2, rightWheelDirection);
 }
 
-void Motors::moveForward(uint8_t speed)
+uint8_t Motors::generatePWMMask(uint8_t wantedBit, uint8_t mask)
+{
+    // Mask (PORTX &= 11100011)
+    if (mask == _masque000)
+        return (~((1 << wantedBit) | (1 << (wantedBit + 1)) | (1 << (wantedBit + 2))));
+    // Mask (PORTX |= XXX001XX)
+    else if (mask == _masque001)
+        return (1 << wantedBit);
+    // Mask (PORTX |= XXX011XX)
+    else if (mask == _masque011)
+        return ((1 << wantedBit) | (1 << (wantedBit + 1)));
+    // Mask (PORTX |= XXX101X)
+    else
+        return ((1 << wantedBit) | (1 << (wantedBit + 2)));
+}
+
+void Motors::selectPWMFrequency(Frequency frequency)
+{
+    switch (frequency)
+    {
+    case Frequency::f15HZ:
+        TCCR0B &= generatePWMMask(CS10, _masque000);
+        TCCR0B |= generatePWMMask(CS10, _masque101);
+        break;
+
+    case Frequency::f61HZ:
+        TCCR0B &= generatePWMMask(CS10, _masque000);
+        TCCR0B |= generatePWMMask(CS12, _masque001);
+        break;
+
+    case Frequency::f245HZ:
+        TCCR0B &= generatePWMMask(CS10, _masque000);
+        TCCR0B |= generatePWMMask(CS10, _masque011);
+        break;
+
+    case Frequency::f1961HZ:
+        TCCR0B &= generatePWMMask(CS10, _masque000);
+        TCCR0B |= generatePWMMask(CS11, _masque001);
+        break;
+
+    case Frequency::f15686HZ:
+        TCCR0B &= generatePWMMask(CS10, _masque000);
+        TCCR0B |= generatePWMMask(CS10, _masque001);
+        break;
+    }
+}
+
+void Motors::adjustSpeed(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed)
+{
+    OCR0A = leftWheelSpeed;
+    OCR0B = rightWheelSpeed;
+}
+
+void Motors::moveForward(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed)
 {
     chooseMovement(_forward, _forward);
-    //select frequency
-    //adjust speed
+    adjustSpeed(leftWheelSpeed, rightWheelSpeed);
 }
 
-void Motors::moveBackward(uint8_t speed)
+void Motors::moveBackward(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed)
 {
     chooseMovement(_backward, _backward);
+    adjustSpeed(leftWheelSpeed, rightWheelSpeed);
 }
 
-void Motors::turnLeft(uint8_t speed)
+void Motors::turnLeft(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed)
 {
     chooseMovement(_backward, _forward);
+    adjustSpeed(leftWheelSpeed, rightWheelSpeed);
 }
 
-void Motors::turnRight(uint8_t speed)
+void Motors::turnRight(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed)
 {
     chooseMovement(_forward, _backward);
+    adjustSpeed(leftWheelSpeed, rightWheelSpeed);
 }
--- library_refactored/Motors.h
+++ library_refactored/Motors.h
@@ -1,17 +1,37 @@
 #include "IOPorts.h"
 
+enum class Frequency
+    {
+        f15HZ,
+        f61HZ,
+        f245HZ,
+        f1961HZ,
+        f15686HZ
+    };
+
 class Motors : public IOPorts
 {
 public:
-    virtual void setIOMode(uint8_t wantedMode);
+    Motors(Frequency frequency);
+    virtual ~Motors();
 
-    void moveForward(uint8_t speed);
-    void moveBackward(uint8_t speed);
-    void turnLeft(uint8_t speed);
-    void turnRight(uint8_t speed);
+    void chooseMovement(uint8_t leftWheelDirection, uint8_t rightWheelDirection);
+    void selectPWMFrequency(Frequency frequency);
+
+    //speed values range from 0 to 255, the bigger the faster. 
+    //Values from 0 to 70 might not do any movement if the friction of the wheels is too high
+    //the speed of the wheels can be chosen indepently
+    void adjustSpeed(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed);
+
+    //practical fonctions for client classes
+    void moveForward(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed);
+    void moveBackward(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed);
+    void turnLeft(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed);
+    void turnRight(uint8_t leftWheelSpeed, uint8_t rightWheelSpeed);
 
 protected:
-    void chooseMovement(uint8_t leftWheelDirection, uint8_t rightWheelDirection);
+    virtual void setIOMode(uint8_t wantedMode);
+    uint8_t generatePWMMask(uint8_t wantedBit, uint8_t mask);
     
     volatile uint8_t *_port = &PORTB;
     volatile uint8_t *_ddr = &DDRB;
@@ -20,4 +40,10 @@ protected:
 private:
     uint8_t _forward = 0;
     uint8_t _backward = 1;
+
+    uint8_t _masque000 = 0;
+    uint8_t _masque001 = 1;
+    uint8_t _masque011 = 2;
+    uint8_t _masque101 = 3;
+    
 };
