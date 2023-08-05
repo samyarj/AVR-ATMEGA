4700fc211ef8ddea86746f8f75397ea1d20264b7
samyarj <112454395+samyarj@users.noreply.github.com>
2023-08-05 16:51:56 -0400

    implementing last `strategy`

--- app-refactored/detection-strategies/Strategy16-3.cpp
+++ app-refactored/detection-strategies/Strategy16-3.cpp
@@ -3,6 +3,23 @@
 
 void Strategy16X3Y::execute(Deplacement& deplacement) override 
 {
-
-
+    _delay_ms(500);
+    deplacement.tournerDroite90();
+    deplacement.changerAngle(-90);
+    _delay_ms(500);
+    deplacement.avancerVersPoto();
+    for (uint8_t i = 0; i < 4; i++)
+    {
+      _delay_ms(500);
+      deplacement.avancerVersPotoD();
+      if (deplacement.obtenirResultat() > 110)
+      {
+        return;
+      }
+    }
+    if (deplacement.obtenirResultat() < 110)
+    {
+      detecte = false;
+    }
+    return;
 }
